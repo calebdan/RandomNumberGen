031fb9520d8c4d1479dc3effe38e9236467e4126
Caleb Tafawa-balewa <tafawabalewacaleb@gmail.com>
2023-10-30 02:23:29 +0000

    initial try

--- app/src/main/java/co/danjuma/randomnumbergen/data/RandomViewModel.kt
+++ app/src/main/java/co/danjuma/randomnumbergen/data/RandomViewModel.kt
@@ -1,13 +1,12 @@
 package co.danjuma.randomnumbergen.data
 
 import androidx.compose.runtime.mutableStateOf
-import androidx.lifecycle.LiveData
-import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import kotlin.random.Random
 
 
 class RandomViewModel : ViewModel() {
+
     private val _randomNumber = mutableStateOf(0)
 
 
